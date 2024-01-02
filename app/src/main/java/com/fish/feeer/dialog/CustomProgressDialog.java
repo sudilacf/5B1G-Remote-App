15faf48278d88bc8db70061c7e8412e3170439a4
sudilacf <sudilacf@gmail.com>
2024-01-02 23:23:34 +0800

    save 1

---
+++ app/src/main/java/com/fish/feeer/dialog/CustomProgressDialog.java
@@ -0,0 +1,64 @@
+package com.fish.feeer.dialog;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import com.fish.feeer.databinding.DialogProgressCustomBinding;
+
+public class CustomProgressDialog {
+
+    private final Context context;
+    private final String message;
+    private final boolean cancelable;
+
+    private DialogProgressCustomBinding binding;
+    private AlertDialog dialog;
+
+    private CustomProgressDialog(Builder builder) {
+        this.context = builder.context;
+        this.message = builder.message;
+        this.cancelable = builder.cancelable;
+        binding = DialogProgressCustomBinding.inflate(LayoutInflater.from(context));
+        binding.message.setText(this.message);
+        dialog = new AlertDialog.Builder(context)
+                .setView(binding.getRoot())
+                .setCancelable(this.cancelable)
+                .create();
+    }
+
+    public void show() {
+        dialog.show();
+    }
+
+    public void dismiss() {
+        dialog.dismiss();
+    }
+
+    public static class Builder {
+
+        private Context context;
+        private String message;
+        private boolean cancelable = true;
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        public Builder setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder setCancelable(boolean cancelable) {
+            this.cancelable = cancelable;
+            return this;
+        }
+
+        public CustomProgressDialog build() {
+
+        }
+
+    }
+
+}
